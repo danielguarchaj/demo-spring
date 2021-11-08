056f21cac0f05bef92f2cac33ca23652468271a8
DanielGuarchaj <gtdanniel@gmail.com>
2021-11-08 13:02:47 -0600

    Implement post, put and delete methods

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -22,4 +25,49 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
+    private boolean emailIsTaken(String email) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+        return studentOptional.isPresent();
+    }
+
+    public void addNewStudent(Student student) {
+        if (emailIsTaken(student.getEmail())) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("student with id " + studentId + " does not exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("student with id " + studentId + " does not exist"));
+
+        if (
+                name != null &&
+                name.length() > 0 &&
+                !Objects.equals(student.getName(), name)
+        ) {
+            student.setName(name);
+        }
+
+        if (
+                email != null &&
+                email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)
+        ) {
+            if (emailIsTaken(email)) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
+
+    }
 }
